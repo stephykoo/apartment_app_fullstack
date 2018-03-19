e795932d1c3053037bb07066cc02a261d4a57e7a
learn <learn@learn18.localdomain>
2018-03-19 12:01:33 -0700

    Update apt controller and model specs

--- apt_app_backend/spec/requests/apartments_spec.rb
+++ apt_app_backend/spec/requests/apartments_spec.rb
@@ -7,6 +7,7 @@ describe 'Apartments API' do
       street_1: '19806 NW Dorchester Way',
       city: 'Hillsboro',
       state: 'OR',
+      postal_code: '12345',
       country: 'USA',
       name: 'Jim Bob',
       phone_number: '610-555-5555',
@@ -27,6 +28,7 @@ describe 'Apartments API' do
         street_1: '22244 twentyfour street',
         city: 'Gnight',
         state: 'CA',
+        postal_code: '12345',
         country: 'USA',
         name: 'Stephy',
         phone_number: '123-456-7890',
@@ -42,12 +44,119 @@ describe 'Apartments API' do
     expect(new_apartment.name).to eq('Stephy')
   end
 
+  it "doesn't create a apartment without street_1" do
+    apartment_params = {
+      apartment: {
+        street_2: 'Unit 211',
+        city: 'A City',
+        state: 'CA',
+        postal_code: '12345',
+        country: 'USA',
+        name: 'Stephy',
+        phone_number: '111-111-1111',
+        contact_hours: 'Monday 12pm'
+      }
+    }
+    post '/apartments', params: apartment_params
+
+    expect(response.status).to eq 422
+
+    json = JSON.parse(response.body)
+
+    expect(json['street_1']).to include "can't be blank"
+  end
+
+  it "doesn't create a apartment without city" do
+    apartment_params = {
+      apartment: {
+        street_1: '211 Street',
+        state: 'CA',
+        postal_code: '12345',
+        country: 'USA',
+        name: 'Stephy',
+        phone_number: '111-111-1111',
+        contact_hours: 'Monday 12pm'
+      }
+    }
+    post '/apartments', params: apartment_params
+
+    expect(response.status).to eq 422
+
+    json = JSON.parse(response.body)
+
+    expect(json['city']).to include "can't be blank"
+  end
+
+  it "doesn't create a apartment without state" do
+    apartment_params = {
+      apartment: {
+        street_1: '211 Street',
+        city: 'City',
+        postal_code: '12345',
+        country: 'USA',
+        name: 'Stephy',
+        phone_number: '111-111-1111',
+        contact_hours: 'Monday 12pm'
+      }
+    }
+    post '/apartments', params: apartment_params
+
+    expect(response.status).to eq 422
+
+    json = JSON.parse(response.body)
+
+    expect(json['state']).to include "can't be blank"
+  end
+
+  it "doesn't create a apartment without postal_code" do
+    apartment_params = {
+      apartment: {
+        street_1: '211 Street',
+        city: 'City',
+        state: 'State',
+        country: 'USA',
+        name: 'Stephy',
+        phone_number: '111-111-1111',
+        contact_hours: 'Monday 12pm'
+      }
+    }
+    post '/apartments', params: apartment_params
+
+    expect(response.status).to eq 422
+
+    json = JSON.parse(response.body)
+
+    expect(json['postal_code']).to include "can't be blank"
+  end
+
+  it "doesn't create a apartment without country" do
+    apartment_params = {
+      apartment: {
+        street_1: '211 Street',
+        city: 'City',
+        state: 'CA',
+        postal_code: '12345',
+        name: 'Stephy',
+        phone_number: '111-111-1111',
+        contact_hours: 'Monday 12pm'
+      }
+    }
+    post '/apartments', params: apartment_params
+
+    expect(response.status).to eq 422
+
+    json = JSON.parse(response.body)
+
+    expect(json['country']).to include "can't be blank"
+  end
+
   it "doesn't create a apartment without a name" do
     apartment_params = {
       apartment: {
         street_1: 'A street',
         city: 'A City',
         state: 'CA',
+        postal_code: '12345',
         country: 'USA',
         phone_number: '111-111-1111',
         contact_hours: 'Monday 12pm'
@@ -60,5 +169,47 @@ describe 'Apartments API' do
     json = JSON.parse(response.body)
 
     expect(json['name']).to include "can't be blank"
+  end
+
+  it "doesn't create a apartment without phone number" do
+    apartment_params = {
+      apartment: {
+        street_1: '211 Street',
+        city: 'City',
+        state: 'CA',
+        postal_code: '12345',
+        country: 'USA',
+        name: 'Stephy',
+        contact_hours: 'Monday 12pm'
+      }
+    }
+    post '/apartments', params: apartment_params
+
+    expect(response.status).to eq 422
+
+    json = JSON.parse(response.body)
+
+    expect(json['phone_number']).to include "can't be blank"
+  end
+
+  it "doesn't create a apartment without contact_hours" do
+    apartment_params = {
+      apartment: {
+        street_1: '211 Street',
+        city: 'City',
+        state: 'CA',
+        postal_code: '12345',
+        country: 'USA',
+        name: 'Stephy',
+        phone_number: '111-111-1111'
+      }
+    }
+    post '/apartments', params: apartment_params
+
+    expect(response.status).to eq 422
+
+    json = JSON.parse(response.body)
+
+    expect(json['contact_hours']).to include "can't be blank"
   end
 end
