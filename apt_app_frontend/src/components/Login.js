e51ee4ffa353a43c7bd4e49bba66af328bbad3a0
learn <learn@learn18.localdomain>
2018-03-20 14:07:15 -0700

    Auth service

--- apt_app_frontend/src/components/Login.js
+++ apt_app_frontend/src/components/Login.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import '../css/Login.css';
+import AuthService from '../services/AuthService';
 
 export default class Login extends Component {
   constructor(){
     super()
+    this.Auth = new AuthService()
     this.state={
       email: '',
       password: ''
@@ -14,12 +16,22 @@ export default class Login extends Component {
     this.setState({ [e.target.name]: e.target.value })
   }
 
+  handleFormSubmit(e){
+    e.preventDefault()
+    this.Auth.login(this.state.email,this.state.password)
+    .then(res =>{
+      this.props.history.replace('/')
+    })
+    .catch(err =>{ alert(err) })
+  }
+
   render() {
     return (
       <div className="center">
         <div className="card">
           <h1>Login</h1>
-          <form>
+          <form onSubmit={this.handleFormSubmit.bind(this)}
+          >
             <input
               className="form-item"
               placeholder="email goes here..."
