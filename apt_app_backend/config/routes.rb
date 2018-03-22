05f21e0b8af6697dbce0416d01c804e0e7faa995
learn <learn@learn18.localdomain>
2018-03-22 13:49:24 -0700

    Landlord controller

--- apt_app_backend/config/routes.rb
+++ apt_app_backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'landlord/index'
+
+  get 'landlord/update'
+
   root "maintenances#index"
 
   resources :maintenances, defaults: {format: :json}
@@ -6,4 +10,8 @@ Rails.application.routes.draw do
   resources :users, defaults: {format: :json}
   resources :apartments, defaults: {format: :json}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "landlord" => "landlord#index"
+  put "landlord/:id" => "landlord#update"
+  patch "landlord/:id" => "landlord#update"
 end
