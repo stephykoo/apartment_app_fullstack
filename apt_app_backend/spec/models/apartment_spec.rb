273ffcbda5045bb12e6d6fd6ca12b7fadb7dddb5
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-25 13:01:51 -0700

    Refactor Apartment model specs to use shoulda matchers

--- apt_app_backend/spec/models/apartment_spec.rb
+++ apt_app_backend/spec/models/apartment_spec.rb
@@ -1,45 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
-  it 'should validate street_1' do
-    apartment = Apartment.create
-    expect(apartment.errors[:street_1]).to_not be_empty
-  end
-
-  it 'should validate city' do
-    apartment = Apartment.create
-    expect(apartment.errors[:city]).to_not be_empty
-  end
-
-  it 'should validate state' do
-    apartment = Apartment.create
-    expect(apartment.errors[:state]).to_not be_empty
-  end
-
-  it 'should validate postal_code' do
-    apartment = Apartment.create
-    expect(apartment.errors[:postal_code]).to_not be_empty
-  end
-
-  it 'should validate country' do
-    apartment = Apartment.create
-    expect(apartment.errors[:country]).to_not be_empty
-  end
-
-  it 'should validate name' do
-    apartment = Apartment.create
-    expect(apartment.errors[:name]).to_not be_empty
-  end
-
-  it 'should validate phone_number' do
-    apartment = Apartment.create
-    expect(apartment.errors[:phone_number]).to_not be_empty
-  end
-
-  it 'should validate contact_hours' do
-    apartment = Apartment.create
-    expect(apartment.errors[:contact_hours]).to_not be_empty
-  end
+  it { should validate_presence_of(:street_1) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_presence_of(:postal_code) }
+  it { should validate_presence_of(:country) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:phone_number) }
+  it { should validate_presence_of(:contact_hours) }
 end
 
 # this is testing the apartment model
