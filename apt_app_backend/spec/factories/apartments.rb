088edd91ce53c3b606ba985b0f9edfe783fedf85
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-25 12:57:27 -0700

    Create Apartment factory

---
+++ apt_app_backend/spec/factories/apartments.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :apartment do
+    street_1      Faker::Address.street_address
+    street_2      Faker::Address.secondary_address
+    city          Faker::Address.city
+    state         Faker::Address.state_abbr
+    postal_code   Faker::Address.zip
+    country       'US'
+    name          Faker::Name.name
+    phone_number  Faker::PhoneNumber.phone_number
+    contact_hours 'Monday thru Friday 9am-5pm'
+    user
+  end
+end
