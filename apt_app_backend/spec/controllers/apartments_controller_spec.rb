f59219d92a28116b2456028ec08fd960fa63d188
Jennifer Hallquist <jnhallquist@gmail.com>
2018-03-25 15:08:08 -0700

    Begin add specs for apartments controller

--- apt_app_backend/spec/controllers/apartments_controller_spec.rb
+++ apt_app_backend/spec/controllers/apartments_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ApartmentsController, type: :controller do
-
+  it { should route(:get, '/apartments').to('apartments#index', format: :json) }
 end
 
 #anything in here is testing the ApartmentsController
